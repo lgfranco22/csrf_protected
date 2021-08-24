1cc222b49ed7ebd7eed3b142805645d2c3d7b882
Luiz Gonzaga Franco Michelmann <39537987+lgfranco22@users.noreply.github.com>
2021-08-24 00:15:32 -0300

    Create csrf_failed.php

---
+++ csrf_failed.php
@@ -0,0 +1,39 @@
+<?php
+//falho
+session_start();
+function getToken()
+{
+    $_SESSION['token'] = hash('sha256', uniqid());
+}
+
+/*
+    $_SESSION['token'] = ( !isset($_SESSION['token']) ) ? getToken() : $_SESSION['token'];
+    variavel    (se isso for verdade) ? faz isso : se nao, faz isso...
+*/
+
+
+    $_SESSION['token'] = ( !isset($_SESSION['token']) ) ? getToken() : $_SESSION['token'];
+
+    if(isset($_POST['nome']) && !empty($_POST['nome']))
+    {
+        if(isset($_REQUEST['token']) && $_REQUEST['token'] != $_SESSION['token'])
+        {
+            header("Content-Type: application/json");
+            echo json_encode(array("status" => "token-ivalid"));       
+            die(
+                header('HTTP/1.0 403 Forbidden')
+            );
+        }
+
+        getToken();
+        
+        echo $_POST['nome']."<br>";
+    }
+?>
+
+<form method="post">
+    <label for="">Nome</label><br>
+    <input type="text" name="nome" autocomplete="off">
+    <input type="hidden" name="token" value="<?php echo $_SESSION['token']; ?>"><br>
+    <button type="submit">Enviar</button>
+</form>
